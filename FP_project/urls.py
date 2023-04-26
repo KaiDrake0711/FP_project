c957ae739adaf079caa011171510f615cd46384e
KaiDrake <kamdenhunter@gmail.com>
2023-04-26 12:39:18 -0400

    yes

--- FP_project/urls.py
+++ FP_project/urls.py
@@ -1,7 +1,8 @@
-"""FP_project URL Configuration
+"""
+FP_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.home_page, name="home"),
 ]
+
+
